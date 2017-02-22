494cf23d50e6b54328a2a84a6aa72f7028f39989
imota <imota@antevenio.com>
2017-02-22 18:01:03 +0100

    alpha!

--- src/Iterator.php
+++ src/Iterator.php
@@ -1,7 +1,99 @@
 <?php
 namespace PdoMysqlSelectIterator;
 
-class Iterator
+class Iterator implements \Iterator
 {
+    const BLOCK_SIZE = 1000;
+    /**
+     * @var \PDO
+     */
+    protected $pdo;
 
+    /**
+     * @var string
+     */
+    protected $query;
+
+    /**
+     * @var int
+     */
+    protected $blockSize;
+
+    /**
+     * @var int
+     */
+    protected $currentBlockIndex;
+
+    /**
+     * @var int
+     */
+    protected $absoluteIndex;
+
+    /**
+     * @var array
+     */
+    protected $results;
+
+    public function __construct(\PDO $pdo, $query, $blockSize = self::BLOCK_SIZE)
+    {
+        $this->pdo = $pdo;
+        $this->query = $query;
+        $this->blockSize = $blockSize;
+        $this->currentBlockIndex = 0;
+        $this->absoluteIndex = 0;
+        $this->results = null;
+    }
+
+    public function next()
+    {
+        $this->absoluteIndex++;
+        $this->currentBlockIndex++;
+        if ($this->endOfBlockReached()) {
+            $this->results = $this->pdo->query($this->getCurrentBlockQuery())->fetchAll();
+            $this->currentBlockIndex = 0;
+        }
+    }
+
+    protected function endOfBlockReached()
+    {
+        return ($this->currentBlockIndex == ($this->blockSize));
+    }
+
+    protected function getCurrentBlockQuery()
+    {
+        return (
+            $this->query .
+            " LIMIT " .
+            $this->pdo->quote($this->blockSize,\PDO::PARAM_INT) .
+            " OFFSET " .
+            $this->pdo->quote($this->absoluteIndex, \PDO::PARAM_INT)
+        );
+    }
+
+    public function key()
+    {
+        return ($this->absoluteIndex);
+    }
+
+    public function current()
+    {
+        return ($this->results[$this->currentBlockIndex]);
+    }
+
+    public function rewind()
+    {
+        if ($this->results == null || $this->absoluteIndex >= $this->blockSize) {
+            $this->absoluteIndex = 0;
+            $this->currentBlockIndex = 0;
+            $this->results = $this->pdo->query($this->getCurrentBlockQuery())->fetchAll();
+        } else {
+            $this->absoluteIndex = 0;
+            $this->currentBlockIndex = 0;
+        }
+    }
+
+    public function valid()
+    {
+        return ($this->results != null && isset($this->results[$this->currentBlockIndex]));
+    }
 }
---
+++ test/IteratorTest.php
@@ -0,0 +1,236 @@
+<?php
+use PdoMysqlSelectIterator\Iterator;
+use PHPUnit\Framework\TestCase;
+
+class IteratorTest extends TestCase
+{
+    protected $query;
+    /**
+     * @var \PDO | \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $pdo;
+    /**
+     * @var Iterator
+     */
+    protected $sut;
+    /**
+     * @var int
+     */
+    protected $blockSize;
+
+    public function setUp()
+    {
+        $this->query = "SELECT * FROM TABLE";
+        $this->pdo = $this->createMock(\PDO::class);
+        $this->blockSize = 3;
+        $this->sut = new Iterator($this->pdo, $this->query, $this->blockSize);
+    }
+
+    protected function setPdoQueryExpectations($queryParams)
+    {
+        $j = 0;
+        foreach($queryParams as $params) {
+            $blockSize = $params[0];
+            $offset = $params[1];
+            $returnedData = $params[2];
+            $this->pdo->expects($this->at($j++))
+                ->method("quote")
+                ->with($this->equalTo($blockSize), $this->equalTo(\PDO::PARAM_INT))
+                ->will($this->returnValue($blockSize));
+            $this->pdo->expects($this->at($j++))
+                ->method("quote")
+                ->with($this->equalTo($offset), $this->equalTo(\PDO::PARAM_INT))
+                ->will($this->returnValue($offset));
+
+            $pdoStatement = $this->createMock(PDOStatement::class);
+
+            $this->pdo->expects($this->at($j++))
+                ->method("query")
+                ->with($this->equalTo(
+                    "SELECT * FROM TABLE LIMIT $blockSize OFFSET $offset"))
+                ->will($this->returnValue($pdoStatement));
+
+            $pdoStatement->expects($this->once())
+                ->method("fetchAll")
+                ->will($this->returnValue($returnedData));
+        }
+    }
+
+    public function testFirstRewindShouldRunLimitQuery()
+    {
+        $this->setPdoQueryExpectations(
+            [
+                [3, 0, [
+                    ["a" => "a1", "b" => "b1"],
+                    ["a" => "a2", "b" => "b2"],
+                    ["a" => "a3", "b" => "b3"]]],
+            ]
+        );
+        $this->sut->rewind();
+    }
+
+    public function testRewindShouldNotRunQueryIfAlreadyOnFirstBlock()
+    {
+        $this->setPdoQueryExpectations(
+            [
+                [3, 0, [
+                    ["a" => "a1", "b" => "b1"],
+                    ["a" => "a2", "b" => "b2"],
+                    ["a" => "a3", "b" => "b3"]]],
+            ]
+        );
+        $this->sut->rewind();
+        $this->pdo->expects($this->never())
+            ->method("query");
+        $this->sut->rewind();
+    }
+
+    public function testRewindShouldRunQueryIfPastFirstBlock()
+    {
+        $this->setPdoQueryExpectations(
+            [
+                [3, 0, [
+                    ["a" => "a1", "b" => "b1"],
+                    ["a" => "a2", "b" => "b2"],
+                    ["a" => "a3", "b" => "b3"]]],
+                [3, 3, [
+                    ["a" => "a4", "b" => "b4"],
+                    ["a" => "a5", "b" => "b5"],
+                    ["a" => "a6", "b" => "b6"]]],
+                [3, 0, [
+                    ["a" => "a1", "b" => "b1"],
+                    ["a" => "a2", "b" => "b2"],
+                    ["a" => "a3", "b" => "b3"]]]
+            ]
+        );
+        $this->sut->rewind();
+        for ($i = 0; $i < 3; $i++) {
+            $this->sut->next();
+        }
+        $this->sut->rewind();
+    }
+
+    public function testNextShouldRunQueriesWhenReachingNewBlocks()
+    {
+        $this->setPdoQueryExpectations(
+            [
+                [3, 0, [
+                    ["a" => "a1", "b" => "b1"],
+                    ["a" => "a2", "b" => "b2"],
+                    ["a" => "a3", "b" => "b3"]]],
+                [3, 3, [
+                    ["a" => "a4", "b" => "b4"],
+                    ["a" => "a5", "b" => "b5"],
+                    ["a" => "a6", "b" => "b6"]]],
+                [3, 6, [
+                    ["a" => "a7", "b" => "b7"],
+                    ["a" => "a8", "b" => "b8"],
+                    ["a" => "a9", "b" => "b9"]]]
+            ]
+        );
+        $this->sut->rewind();
+        for ($i = 0; $i < 6; $i++) {
+            $this->sut->next();
+        }
+    }
+
+
+    public function testKeyShouldReturnCurrentIndex()
+    {
+        $this->setPdoQueryExpectations(
+            [
+                [3, 0, [
+                    ["a" => "a1", "b" => "b1"],
+                    ["a" => "a2", "b" => "b2"],
+                    ["a" => "a3", "b" => "b3"]]],
+                [3, 3, [
+                    ["a" => "a4", "b" => "b4"],
+                    ["a" => "a5", "b" => "b5"],
+                    ["a" => "a6", "b" => "b6"]]]
+            ]
+        );
+        $this->sut->rewind();
+        for ($i = 0; $i < 5; $i++) {
+            $this->assertEquals($i, $this->sut->key());
+            $this->sut->next();
+        }
+    }
+
+    public function testRewindShouldResetCurrentIndex()
+    {
+        $this->setPdoQueryExpectations(
+            [
+                [3, 0, [
+                    ["a" => "a1", "b" => "b1"],
+                    ["a" => "a2", "b" => "b2"],
+                    ["a" => "a3", "b" => "b3"]]]
+            ]
+        );
+        $this->sut->rewind();
+        $this->assertEquals(0, $this->sut->key());
+        $this->sut->next();
+        $this->sut->rewind();
+        $this->assertEquals(0, $this->sut->key());
+    }
+
+    public function testCurrentShouldReturnCurrentRow()
+    {
+        $this->setPdoQueryExpectations(
+            [
+                [3, 0, [
+                    ["a" => "a1", "b" => "b1"],
+                    ["a" => "a2", "b" => "b2"],
+                    ["a" => "a3", "b" => "b3"]]],
+                [3, 3, [
+                    ["a" => "a4", "b" => "b4"],
+                    ["a" => "a5", "b" => "b5"],
+                    ["a" => "a6", "b" => "b6"]]],
+                [3, 6, [
+                    ["a" => "a7", "b" => "b7"],
+                    ["a" => "a8", "b" => "b8"],
+                    ["a" => "a9", "b" => "b9"]]]
+            ]
+        );
+        $this->sut->rewind();
+        for ($i = 0; $i < 8; $i++) {
+            $this->assertEquals(
+                ["a" => "a".($i+1), "b" => "b".($i+1)], $this->sut->current()
+            );
+            $this->sut->next();
+        }
+    }
+
+    public function testValidShouldReturnTrueWhenRowExists()
+    {
+        $this->setPdoQueryExpectations(
+            [
+                [3, 0, [
+                    ["a" => "a1", "b" => "b1"],
+                    ["a" => "a2", "b" => "b2"],
+                    ["a" => "a3", "b" => "b3"]]],
+                [3, 3, [
+                    ["a" => "a4", "b" => "b4"],
+                    ["a" => "a5", "b" => "b5"],
+                    ["a" => "a6", "b" => "b6"]]],
+            ]
+        );
+        $this->sut->rewind();
+        for ($i = 0; $i < 3; $i++) {
+            $this->assertTrue($this->sut->valid());
+            $this->sut->next();
+        }
+    }
+
+    public function testValidShouldReturnFalseWhenRowDoesntExist()
+    {
+        $this->setPdoQueryExpectations(
+            [
+                [3, 0, [
+                    ["a" => "a1", "b" => "b1"]]]
+            ]
+        );
+        $this->sut->rewind();
+        $this->sut->next();
+        $this->assertFalse($this->sut->valid());
+    }
+}
