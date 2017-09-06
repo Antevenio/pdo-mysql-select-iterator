8d19419bb379c66e5676ccc0ccbd2758e683bdb6
imota <imota@antevenio.com>
2017-09-06 10:53:47 +0200

    Do not return a LimitIterator if the query doesn't have an order by clause.

--- src/Factory.php
+++ src/Factory.php
@@ -10,7 +10,7 @@ class Factory {
      */
     public function create(\PDO $adapter, $query, $blockSize)
     {
-        if ($this->queryIsLimitable($query) || $blockSize == 0) {
+        if ($this->queryIsLimitable($query) && $blockSize > 0) {
             return new LimitIterator($adapter, $query, $blockSize);
         } else {
             $adapter->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
@@ -25,7 +25,9 @@ class Factory {
     }
 
     protected function queryIsLimitable($query) {
-        return !$this->queryHasALimitClause($query) && !$this->queryHasARandFunction($query);
+        return !$this->queryHasALimitClause($query)
+            && !$this->queryHasARandFunction($query)
+            && $this->queryHasOrderByClause($query);
     }
 
     protected function queryHasALimitClause($query)
@@ -36,5 +38,10 @@ class Factory {
     protected function queryHasARandFunction($query)
     {
         return (preg_match('/RAND\(.*\)/i', $query));
+    }
+
+    protected function queryHasOrderByClause($query)
+    {
+        return (preg_match('/ORDER BY/i', $query));
     }
 }
---
+++ test/FactoryTest.php
@@ -0,0 +1,55 @@
+<?php
+use PdoMysqlSelectIterator\LimitIterator;
+use PdoMysqlSelectIterator\NativePDOIterator;
+use PHPUnit\Framework\TestCase;
+
+class FactoryTest extends TestCase
+{
+    /**
+     * @var \PdoMysqlSelectIterator\Factory
+     */
+    protected $sut;
+    /**
+     * @var PDO | PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $pdoMock;
+
+    public function setUp()
+    {
+        $this->sut = new \PdoMysqlSelectIterator\Factory();
+        $this->pdoMock = $this->createMock(PDO::class);
+
+        $this->pdoMock->expects($this->any())
+            ->method("query")
+            ->will($this->returnValue(new PDOStatement()));
+    }
+
+    public function dataProvider() {
+        return [
+            ["select * from foo", PDOStatement::class],
+            ["select * from foo order by a", LimitIterator::class],
+            ["select * from foo limit 100", PDOStatement::class],
+            ["select * from foo limit 100 order by a", PDOStatement::class],
+            ["select a,rand() from foo order by a", PDOStatement::class]
+        ];
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testShouldReturnTheProperIteratorType($query, $expectedClass)
+    {
+        $this->assertInstanceOf($expectedClass, $this->sut->create($this->pdoMock, $query, 100));
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testShouldReturnNativeIteratorWhenBlockSizeIsZero($query)
+    {
+        $this->assertInstanceOf(
+            PDOStatement::class,
+            $this->sut->create($this->pdoMock, $query, 0)
+        );
+    }
+}
